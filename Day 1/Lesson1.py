255bb7ded7190bca8b79db1dc813a15ebcba636c
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-09-01 19:04:53 +0400

    Second commit

---
+++ Day 1/Lesson1.py
@@ -0,0 +1,13 @@
+#information about me
+name = "Nika"
+surname = "Malakmadze"
+age = 15
+weight = 51.10
+height = 169.15
+country = "Georgia"
+city = "Batumi"
+joined_Goa = True
+
+#print info in terminal
+print("Hello world! My name is " + name + " " + surname + ", I am " + str(age) + " years old, from " + country + "," + city + ". I am " + 
+      str(height) + "cm tall and weight " + str(weight) + " kilos." + " have i joined Goa Academy?" + " answer is:" + " " + str(joined_Goa))
