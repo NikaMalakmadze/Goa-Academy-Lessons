6c70cef5bfe5b1287ddb4443dd2bdcd672e342dc
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-09-13 13:37:19 +0400

    Level3 Homework

---
+++ Level3/Homework/Task.py
@@ -0,0 +1,37 @@
+#Player stats
+
+score = 1
+
+health = 85.5
+
+attacking_points = 20
+
+defence_points = 25
+
+math_score = 10
+english_score = 10
+science_score = 10
+chemistry_score = 10
+history_score = 10
+
+#Info about me
+
+name = "Nika"
+surname = "Malakmadze"
+age = 15
+
+#Display everything on terminal
+
+print("---------------------------------------------------------")
+print(f"My Player score is: {score} Point(s), his health is {health} HP")
+print(f"Attacking strenght: {attacking_points}")
+print(f"Armor Defence: {defence_points}%")
+print("---------------------------------------------------------")
+print(f"Hi i am {name} {surname}! i am {age}")
+print(f"My grades at school:")
+print(f"Math: {math_score}")
+print(f"English: {english_score}")
+print(f"Science: {science_score}")
+print(f"Chemistry: {chemistry_score}")
+print(f"History: {history_score}")
+print("---------------------------------------------------------")
