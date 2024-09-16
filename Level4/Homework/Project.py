cd6a2abcb5af7decf836e37bd0ddb6af04179efd
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-09-16 21:23:15 +0400

    Level4

---
+++ Level4/Homework/Project.py
@@ -0,0 +1,55 @@
+
+#task 1:
+
+print("----------------------------TASK 1-------------------------------")
+
+name = input("Your name: ")
+surname = input("Your surname")
+age = input("Your age: ")
+
+print(f"{name} {surname} is {age} years old")
+
+#task 2:
+
+print("----------------------------TASK 2-------------------------------")
+
+num1 = int(input())
+num2 = int(input())
+
+print(f"sum of this two number: {num1 + num2}")
+print(f"difference of this two number: {num1 - num2}")
+print(f"Multiply  of this two number: {num1 * num2}")
+print(f"divide second number on first number: {float(num1 / num2)}")
+
+#task 3:
+
+print("----------------------------TASK 3-------------------------------")
+
+user1 = input("enter your name: ")
+print(user1)
+
+#task 4:
+
+print("----------------------------TASK 4-------------------------------")
+
+math = 20 + 20
+math2 = math - 10
+math3 = math * 2
+print(math3)
+
+#task 5:
+
+print("----------------------------TASK 5-------------------------------")
+
+input_academy = input("Please enter name of your academy:")
+input_school = input("Please enter name of your school: ")
+input_name = input("Please enter your name: ")
+input_lastname = input("Please enter your last name: ")
+input_age = input("Please enter your age: ")
+
+print("-----------------INFO------------------")
+print(input_academy)
+print(input_school)
+print(input_name)
+print(input_lastname)
+print(input_age)
