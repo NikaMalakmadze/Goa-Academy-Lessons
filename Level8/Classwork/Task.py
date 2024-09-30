3b24f588dde13b916ecdae2576b0542117634e1c
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-09-30 20:58:07 +0400

    Level8

---
+++ Level8/Classwork/Task.py
@@ -0,0 +1,28 @@
+
+a = 4
+
+b = 10
+
+c = 20
+
+d = 15
+
+print(f"{a} > {b} answer: {a > b}")
+print(f"{a} < {b} answer: {a < b}")
+print(f"{a} >= {b} answer: {a >= b}")
+print(f"{a} <= {b} answer: {a <= b}")
+print(f"{a} == {b} answer: {a == b}")
+print(f"{a} != {b} answer: {a != b}")
+print(f"{c} >  {d} answer: {a > b}")
+print(f"{c} <  {d} answer: {a < b}")
+print(f"{c} >= {d} answer: {a >= b}")
+print(f"{c} <= {d} answer: {a <= b}")
+print(f"{c} == {d} answer: {a == b}")
+print(f"{c} != {d} answer: {a != b}")
+
+misha = 20
+
+grisha = 10
+
+print(f"grisha metia mishaze?: {grisha > misha}")
+print(f"misha metia grishaze:?: {misha > grisha}")
