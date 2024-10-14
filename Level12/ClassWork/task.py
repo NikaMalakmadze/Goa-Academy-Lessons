fefe63e063c09b6756a440f0d4db24e764617204
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-10-14 21:03:08 +0400

    Level12 ClassWork

---
+++ Level12/ClassWork/task.py
@@ -0,0 +1,26 @@
+
+#task 1 even
+
+for i in range(0 , 10000 , 2):
+    print(i)
+
+#task 2 odd
+
+for i in range(1 , 10000 , 2):
+    print(i)
+
+#task 3 even
+
+n = 0
+
+while n < 10000:
+    print(n)
+    n += 2
+
+#task 4 odd
+
+n = 1
+
+while n < 10000:
+    print(n)
+    n += 2
