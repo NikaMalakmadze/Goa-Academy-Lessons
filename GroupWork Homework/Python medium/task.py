ee891be217cf0fa70c7ee1392b00eea2a925356b
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-10-18 22:55:32 +0400

    GroupWorkHomeWork

---
+++ GroupWork Homework/Python medium/task.py
@@ -0,0 +1,14 @@
+
+#task 5
+
+age = int(input("Please enter your age: "))
+
+running = True
+
+while age >= 10 and running:
+    print("You are over 10 years old")
+    running = False
+
+while age <= 10 and running:
+    print("False")
+    running = False
