00f493e75834a3ba58fb49048e7063e7c32d4f49
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-11-05 19:05:00 +0400

    Level18 HomeWork

---
+++ Level18/HomeWork/task.py
@@ -0,0 +1,46 @@
+#task 1
+
+print("-------------TASK-1-------------")
+
+fruits = ["apple", "banana", "orange","lemon"]
+
+print(f"first element: {fruits[0]}")
+print(f"last element: {fruits[-1]}")
+
+#task 2
+
+print("-------------TASK-2-------------")
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f"first element: {arr[0]}")
+print(f"fifth element: {arr[4]}")
+print(f"tenth element: {arr[9]}")
+
+#task 3
+
+print("-------------TASK-3-------------")
+
+animals = ["wolf","tiger","bear","elephant",]
+
+animals[0] = "lion"
+
+for i in animals:
+    print(i)
+
+#task 4
+
+print("-------------TASK-4-------------")
+
+films = ["avengers","spiderman","iron man","fight club"]
+
+print(films[-1])
+
+#task 5
+
+print("-------------TASK-5-------------")
+
+num = [10, 20, 30, 40, 50, 60]
+
+print(num[-1])
+print(num[-2])
