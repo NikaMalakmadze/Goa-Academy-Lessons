be051b515cf20981c05a9c7e1d196b7576225890
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-11-09 14:18:28 +0400

    GroupWork Level14 Changes

---
+++ Level6/Classwork/task.py
@@ -0,0 +1,20 @@
+
+#task 1
+
+name = input()
+
+surname = input()
+
+print(name + surname)
+
+#task 2
+
+name1 = int(input())
+name2 = int(input())
+
+print("sum: " , name1 + name2)
+print("difference: " , name1 - name2)
+print("multiply: " , name1 * name2)
+print("divide: " , name1 / name2 if name2 != 0 else "cannot")
+print("divide "//": " , name1 // name2)
+print("**:" , name1 ** name2)
