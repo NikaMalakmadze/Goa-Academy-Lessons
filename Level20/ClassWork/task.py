5782fc27ec60d567a7cfedd320a22ceb5d49775a
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-11-11 21:20:08 +0400

    Level20 ClassWork

---
+++ Level20/ClassWork/task.py
@@ -0,0 +1,13 @@
+
+#task 1
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+for i in range(len(nums)):
+    if nums[i] % 2 == 1:
+        nums[i] = "odd"
+
+for num in nums:
+    print()
+    print(num, end="")
+    
