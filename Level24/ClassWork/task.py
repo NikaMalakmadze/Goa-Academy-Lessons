db243a39fa7794801f650ba41dd9a2a9e8ee070b
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-11-25 21:02:18 +0400

    Level24 ClassWork

---
+++ Level24/ClassWork/task.py
@@ -0,0 +1,20 @@
+
+#task 1
+
+print("-------------TASK-1-------------")
+
+name = input("Enter Your Name: ")
+
+message = "{} is good and clever boy!".format(name)
+
+print(message)
+
+print("-------------TASK-2-------------")
+
+#task 2
+
+str = "Shaurma ar aris xinkalze kargi".split(" ")
+
+joined = "! ".join(str)
+
+print(joined)
