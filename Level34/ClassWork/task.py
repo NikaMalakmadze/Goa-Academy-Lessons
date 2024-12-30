39d8ac4cc90a91154601a4ddfedcf58dc35db9ab
NikaMalakmadze <malakmadzenika0909@gmail.com>
2024-12-30 21:01:31 +0400

    Level34 ClassWork

---
+++ Level34/ClassWork/task.py
@@ -0,0 +1,10 @@
+
+#task 1
+
+films = ('Inception', 'Amélie', 'The Godfather', 'The Grand Budapest Hotel', 'The Matrix', 'Parasite')
+
+inception, *restfilms = films
+
+#task 2
+
+print(films[0], films[-1])
