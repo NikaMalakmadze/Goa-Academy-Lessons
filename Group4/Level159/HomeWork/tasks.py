8e7f498bf2e8cd39ca23f7d9d792cb76dd3daf90
NikaMalakmadze <malakmadzenika0909@gmail.com>
2025-04-24 21:07:41 +0400

    Level159 HomeWork | V2 Not Finished

--- Group4/Level159/HomeWork/tasks.py
+++ Group4/Level159/HomeWork/tasks.py
@@ -1,6 +1,7 @@
 
 # task 1
 
+#V1
 # works but need more than 12s
 
 def calc_n(ord_max):
@@ -17,4 +18,17 @@ def min_length_num(num_dig, ord_max, n=1):
     if len(str(num)) == num_dig: return [True, n]
     return min_length_num(num_dig, ord_max, n+1)
 
-print(min_length_num(8, 16))
+#V2
+# works, but still need more than 12s
+
+from math import log10
+
+def min_length_num_V2(num_dig, ord_max):
+    for term in range(1, ord_max+1):
+        num = sum((i**(term-i)) for i in range(1, term))
+        if num > 0:
+            digits = int(log10(num)) + 1
+            if digits == num_dig: return [True, term]
+    return [False, -1]
+
+print(min_length_num_V2(8, 16))
